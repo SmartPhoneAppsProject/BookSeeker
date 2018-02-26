3b2e227cd3ede58fbe59c7877da711cdcf9324b9
kawatsu kenya <3982ne@gmail.com>
2018-02-26 14:04:53 +0900

    datePickerIOSの追加

--- entryScreen.js
+++ entryScreen.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react';
-import { StyleSheet, TextInput, View, Text, Image, Button, Dimensions, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+  Image,
+  Button,
+  Dimensions,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  DatePickerIOS
+} from 'react-native';
 
 export default class EntryScreen extends Component {
   static navigationOptions = {
@@ -10,9 +21,23 @@ export default class EntryScreen extends Component {
     super(props);
     this.state = {
       title: '',
+      chosenDate: new Date(),
       tags: '',
       photo: null,
+      showDatePicker: false,
     };
+
+    this.setDate = this.setDate.bind(this);
+    this.setDatePicker = this.setDatePicker.bind(this);
+  }
+
+  //コンストラクタでthisをバインドすることで呼び出し場所に左右されない
+  setDate(newDate) {
+    this.setState({ chosenDate: newDate });
+  }
+
+  setDatePicker() {
+    this.setState({ showDatePicker: !this.state.showDatePicker });
   }
 
   goScanScreen() {
@@ -32,7 +57,7 @@ export default class EntryScreen extends Component {
       ? <Image resizeMode='contain'
         style={styles.photo}
         source={{ uri: this.state.photo.uri }} />
-      : null;
+      : <View />;
     return (
       <View style={styles.photoContainer} >
         {photo}
@@ -44,6 +69,7 @@ export default class EntryScreen extends Component {
   }
 
   renderFormContainer() {
+    const datePicker = this.renderDatePicker();
     return (
       <View style={styles.formContainer}>
         <Text style={styles.tag}>Title</Text>
@@ -64,12 +90,33 @@ export default class EntryScreen extends Component {
           placeholder='tags'
           maxLenghth={100} />
 
+        <View style={styles.datePickerContainer}>
+          <Text style={styles.tag}>Published_at</Text>
+          <TouchableOpacity style={styles.datePicker}
+            onPress={this.setDatePicker}>
+            <Text>{`${this.state.chosenDate}`}</Text>
+          </TouchableOpacity>
+          {datePicker}
+        </View>
+
         <TouchableOpacity style={styles.buttonContainer}
           onPress={this.goScanScreen.bind(this)}>
           <Text style={styles.buttonText}>submit</Text>
         </TouchableOpacity>
       </View>
     );
+  }
+
+  renderDatePicker() {
+    const showDatePicker = this.state.showDatePicker
+      ? <DatePickerIOS
+        date={this.state.chosenDate}
+        onDateChange={this.setDate}
+        mode='date'
+        locale='ja' />
+      : <View />;
+
+    return showDatePicker;
   }
 
   render() {
@@ -123,6 +170,12 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(255,255,255,0.2)',
     marginBottom: 10,
     padding: 10,
+  },
+  datePickerContainer: {
+    flex: 1,
+  },
+  datePicker: {
+    flex: 1,
   },
   buttonContainer: {
     backgroundColor: '#2980b6',
