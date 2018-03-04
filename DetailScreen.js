a4d74c9185f051ed240e175648744d756ee477d5
nomurakkooo <b1519008@planet.kanazawa-it.ac.jp>
2018-03-04 15:44:29 +0900

    indentの修正

--- DetailScreen.js
+++ DetailScreen.js
@@ -82,25 +82,25 @@ export default class DetailScreen extends React.Component {
             </View>
         </View>
         <View style={ [styles.base, styles.statusContainer] }>
-            {this.state.currentStatus ?
-                <Text style={ [styles.status, styles.statusNo] }>貸し出し中</Text>
-                : <Text style={ [styles.status, styles.statusOk] }>貸し出OK</Text>
+            {this.state.currentStatus
+              ? <Text style={ [styles.status, styles.statusNo] }>貸し出し中</Text>
+              : <Text style={ [styles.status, styles.statusOk] }>貸し出OK</Text>
             }
         </View>
         <View style={ [styles.base, styles.titleContainer] }>
-            <Text style={ styles.title}>{params.item.title}</Text>
+            <Text style={styles.title}>{params.item.title}</Text>
         </View>
-        <View style={ styles.infoContainer }>
-            <Text style={ styles.infoHead}>詳細情報</Text>
-            <ScrollView style={ styles.infoBody}>
+        <View style={styles.infoContainer}>
+            <Text style={styles.infoHead}>詳細情報</Text>
+            <ScrollView style={styles.infoBody}>
                 <Text>JANコード：{params.item.jan_code}</Text>
                 <Text>出版日：{params.item.published_at}</Text>
                 <Text>アプリへの追加日：{params.item.created_at}</Text>
             </ScrollView>
         </View>
-        <View style={ [styles.base, styles.buttonContainer]}>
-          {this.state.currentStatus ?
-            <Button title="返却" onPress={this._returnBook} />
+        <View style={ [styles.base, styles.buttonContainer] }>
+          {this.state.currentStatus
+            ? <Button title="返却" onPress={this._returnBook} />
             : <Button title="貸出" onPress={this._lendBook}/>
           }
         </View>
@@ -111,98 +111,98 @@ export default class DetailScreen extends React.Component {
 
 const styles = StyleSheet.create({
   container:
-      {
-          flex: 1,
-          backgroundColor: '#fff',
-      },
+    {
+      flex: 1,
+      backgroundColor: '#fff',
+    },
   base:
-      {
-          justifyContent: 'center',
-          alignItems: 'center',
-      },
+    {
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
   imgContainer:
-      {
-          flex: 1,
-          flexDirection: 'row',
-          marginTop: 20,
-      },
+    {
+      flex: 1,
+      flexDirection: 'row',
+      marginTop: 20,
+    },
   img:
-      {
-          height: 90,
-          width: 90,
-      },
+    {
+      height: 90,
+      width: 90,
+    },
   tagContainer:
-      {
-          height: 40,
-          marginLeft: 40,
-          marginTop: 30,
-      },
+    {
+      height: 40,
+      marginLeft: 40,
+      marginTop: 30,
+    },
   tag:
-      {
-          fontSize: 15,
-      },
+    {
+      fontSize: 15,
+    },
   statusContainer:
-      {
-          flex: 1,
-          flexDirection: 'row',
-          marginTop: 20,
-      },
+    {
+      flex: 1,
+      flexDirection: 'row',
+      marginTop: 20,
+    },
   status:
-      {
-          borderWidth: 1,
-          borderColor: '#999',
-          borderRadius: 5,
-          marginLeft: 40,
-          marginRight: 30,
-          paddingTop: 5,
-          width: 100,
-          height: 30,
-          backgroundColor: '#ccc',
-          textAlign: 'center',
-          overflow: 'hidden',
-      },
+    {
+      borderWidth: 1,
+      borderColor: '#999',
+      borderRadius: 5,
+      marginLeft: 40,
+      marginRight: 30,
+      paddingTop: 5,
+      width: 100,
+      height: 30,
+      backgroundColor: '#ccc',
+      textAlign: 'center',
+      overflow: 'hidden',
+    },
   statusOk:
-      {
-          color: '#008000',
-          borderColor: '#008000'
-      },
+    {
+      color: '#008000',
+      borderColor: '#008000'
+    },
   statusNo:
-      {
-          color: '#ff0000',
-          borderColor: '#ff0000'
-      },
+    {
+      color: '#ff0000',
+      borderColor: '#ff0000'
+    },
   titleContainer:
-      {
-          flex: 1,
-      },
+    {
+      flex: 1,
+    },
   title:
-      {
-          fontSize: 20,
-      },
+    {
+      fontSize: 20,
+    },
   infoContainer:
-      {
-          flex: 3,
-          marginTop: 5,
-          marginLeft: 25,
-          marginRight: 25,
-          marginBottom: 10,
-      },
+    {
+      flex: 3,
+      marginTop: 5,
+      marginLeft: 25,
+      marginRight: 25,
+      marginBottom: 10,
+    },
   infoHead:
-      {
-          fontSize: 18,
-          marginTop: 5,
-          marginLeft: 5,
-          paddingTop: 3,
-          paddingLeft: 5,
-          height: 30,
-          width: 200,
-      },
+    {
+      fontSize: 18,
+      marginTop: 5,
+      marginLeft: 5,
+      paddingTop: 3,
+      paddingLeft: 5,
+      height: 30,
+      width: 200,
+    },
   infoBody:
-      {
-          marginLeft: 20,
-      },
+    {
+      marginLeft: 20,
+    },
   buttonContainer:
-      {
-          flex: 2,
-      },
+    {
+      flex: 2,
+    },
 });
