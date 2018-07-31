30599bbba993a9655694090be552a5ab42988ec5
kawatsu kenya <3982ne@gmail.com>
2018-07-31 18:37:18 +0900

    fix image size

--- src/components/DetailScreen/BookImage.js
+++ src/components/DetailScreen/BookImage.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  View,
-  Image,
-} from 'react-native';
+import { Image } from 'react-native';
 
 import { bookImage as styles } from './Styles';
 
 const BookImage = ({ uri }) => (
-  <View style={styles.container}>
-    <Image
-      style={{ width: 50, height: 50 }}
-      source={{ uri }}
-    />
-  </View>
+  <Image
+    style={styles.image}
+    source={{ uri }}
+  />
 );
 
 BookImage.propTypes = {
